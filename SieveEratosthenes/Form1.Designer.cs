7baf6f8882bd611f71d0a2605d7ff4a1bf148cf1
Vlad_Polygalov <vladpoligalov3@gmail.com>
2021-12-25 14:46:59 +0200

    Initial commit

---
+++ SieveEratosthenes/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace SieveEratosthenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Max Number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Find Primes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 118);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 320);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 150;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Min Number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SieveEratosthenes/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SieveEratosthenes
+{
+    public partial class Form1 : Form
+    {
+        private ulong i = 2;
+        private ulong j;
+        private bool[] nonPrime;
+        private ulong maxNumber;
+        private ulong minNumber;
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            if (textBox2.Text != "")
+            {
+                minNumber = ulong.Parse(textBox2.Text);
+                maxNumber = ulong.Parse(textBox1.Text);
+                EratospheneBetweenTwoIntS();
+            }
+            else
+            {
+                maxNumber = ulong.Parse(textBox1.Text);
+                if (maxNumber <= 300)
+                {
+                    CreateSieve();
+                    Eratosphene(maxNumber);
+                    timer1.Start();
+                }
+                else
+                {
+                    EratospheneForBigNumber();
+                }
+            }
+        }
+
+        private List<ulong> GetPrimes()
+        {
+            List<ulong> primes = new List<ulong>();
+
+            for (int i = 0; i < nonPrime.Length; i++)
+            {
+                if (!nonPrime[i])
+                {
+                    primes.Add((ulong)i);
+                }
+            }
+
+            return primes;
+        }
+
+        private void EratospheneBetweenTwoIntS()
+        {
+            Eratosphene((ulong)Math.Sqrt(maxNumber));
+            List<ulong> primes = GetPrimes();
+            nonPrime = new bool[maxNumber - minNumber + 1];
+
+            foreach (var prime in primes)
+            {
+                ulong firstMultiple = minNumber / prime;
+                if (firstMultiple <= 1)
+                {
+                    firstMultiple = prime + prime;
+                }
+                else if (minNumber % prime != 0)
+                {
+                    firstMultiple = (firstMultiple * prime) + prime;
+                }
+                else
+                {
+                    firstMultiple *= prime;
+                }
+
+                for (ulong j = firstMultiple; j <= maxNumber; j += prime)
+                {
+                    nonPrime[j - minNumber] = true;
+                }
+            }
+            WriteToFile();
+        }
+
+        private void EratospheneForBigNumber()
+        {
+            ulong sqrt = (ulong)Math.Sqrt(maxNumber >> 1);
+            byte[] nonPrime = new byte[(maxNumber >> 4) + 1];
+            ulong len = (ulong)nonPrime.Length << 3;
+            ulong countsOfBits = 0;
+            ulong maxByte = (ulong)((sqrt + 1) >> 3);
+
+            for (ulong i = 0; i < maxByte; i++, countsOfBits += 8)
+            {
+                for (int bitNumber = 0; bitNumber < 8;)
+                {
+                    if ((nonPrime[i] & 1 << bitNumber++) == 0)
+                    {
+                        ulong step = ((countsOfBits + (ulong)bitNumber) << 1) + 1;
+
+                        for (ulong j = (step * step >> 1) - 1; j < len; j += step)
+                        {
+                            nonPrime[j >> 3] |= (byte)(1 << (int)(j % 8));
+                        }
+                    }
+                }
+            }
+            WriteToFile(nonPrime);
+        }
+        private void WriteToFile()
+        {
+            using (StreamWriter write = new StreamWriter("Primes.txt"))
+            {
+                for (ulong i = minNumber; i <= maxNumber; i++)
+                {
+                    if (!nonPrime[i - minNumber])
+                    {
+                        write.Write(i + " ");
+                    }
+                }
+            }
+        }
+
+        private void WriteToFile(byte[] arr)
+        {
+            ulong c = 0;
+            using (StreamWriter write = new StreamWriter("Primes.txt"))
+            {
+                write.WriteLine(2);
+
+                for (int i = 0; i < arr.Length; i++, c += 8)
+                {
+                    for (ulong k = 0; k < 8;)
+                    {
+                        if ((arr[i] & 1 << (int)k++) == 0)
+                        {
+                            if ((k + c) * 2 + 1 <= maxNumber)
+                            {
+                                
+                                write.WriteLine(((k + c) * 2 + 1) + " ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Eratosphene(ulong maxNumber)
+        {
+            uint sqrt = (uint)Math.Sqrt(maxNumber);
+            nonPrime = new bool[maxNumber + 1];
+            nonPrime[0] = nonPrime[1] = true;
+
+            for (uint i = 2; i <= sqrt; i++)
+            {
+                if (!nonPrime[i])
+                {
+                    for (ulong j = i * i; j <= maxNumber; j += i)
+                    {
+                        nonPrime[j] = true;
+                    }
+                }
+            }
+        }
+
+        private void CreateSieve()
+        {
+            for (ulong i = 2; i < maxNumber + 1; i++)
+            {
+                richTextBox1.Text += $"{i} ";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i <= maxNumber && !nonPrime[i])
+            {
+                j = i * i;
+                timer1.Stop();
+                timer2.Start();
+            }
+            else if (i > maxNumber)
+            {
+                timer2.Stop();
+                timer1.Stop();
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (j <= maxNumber && nonPrime[j])
+            {
+                richTextBox1.Select(richTextBox1.Text.IndexOf($"{j}"), j.ToString().Length);
+                richTextBox1.SelectionColor = Color.White;
+                nonPrime[j] = true;
+            }
+            else
+            {
+                i++;
+                timer1.Start();
+                timer2.Stop();
+            }
+            j += i;
+        }
+    }
+}
